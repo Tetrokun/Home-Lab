2e1a7ab0f4b437f281cc1822807ee3966178a8ac
Philip Pasquale <philip.pasquale@gmail.com>
2019-02-01 02:54:05 -0500

    Fixed imports, fixed figlet, implemented colors

--- Main.py
+++ Main.py
@@ -1,17 +1,23 @@
 import os
 import socket
-import platform
-from art import *
-from termcolor import colored
-from colorama import *
-init()
+import sys
+from colored import fg, bg, attr
+from pyfiglet import print_figlet
+
 Hostname = socket.gethostname()
 if os.name == 'nt':
     OS = "Windows"
 elif os.name =='posix':
     OS = "Linux"
+if OS=="Windows":
+    print_figlet('Windows script',font="standard", colors='255;165;0:')
+if OS=="Linux" :
+    print_figlet('Linux Script',font="slant", colors='255;90;0:')
+    print_figlet(Hostname,font="slant", colors='255;90;0:')
 
-tprint(colored(Hostname, 'green', 'on_red'))
+
+
+
 
 
 
